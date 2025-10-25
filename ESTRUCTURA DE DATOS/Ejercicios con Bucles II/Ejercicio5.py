ab020fee3cf9ac4924fce130e85890a6b4063caf
dmonterozoria <montero272009>
2025-10-25 18:48:17 -0400

    Bucles II

--- ESTRUCTURA DE DATOS/Ejercicios con Bucles II/Ejercicio5.py
+++ ESTRUCTURA DE DATOS/Ejercicios con Bucles II/Ejercicio5.py
@@ -1,7 +1,7 @@
 #Pide 5 notas, calcula la suma y el promedio final.
 suma = 0
 for i in range(5):
-    nota = float(input(f"Ingrese la nota #{i + 1}: "))
+    nota = float(input(f"Ingrese la nota {i + 1}: "))
     suma = suma + nota
 promedio = suma / 5
 print(f"La suma de las notas es: {suma} y el promedio final es: {promedio}")
